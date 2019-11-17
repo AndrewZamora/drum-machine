420092043c140ab2173190188c3f9693232cae06
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-11-17 19:53:32 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
+const DrumPad = props => {
+  const { func } = props;
+  const drumBtns = [
+    {
+      name: "Q"
+    },
+    {
+      name: "W"
+    },
+    {
+      name: "E"
+    },
+    {
+      name: "A"
+    },
+    {
+      name: "S"
+    },
+    {
+      name: "D"
+    },
+    {
+      name: "Z"
+    },
+    {
+      name: "X"
+    },
+    {
+      name: "C"
+    }
+  ]
+  const pad = drumBtns.map((btn, index) => {
+    return (
+      <button className="drum-pad" id={btn.name + "_btn"} key={index + btn.name} onClick={func(btn.name + "_ref")}>
+        {btn.name}
+        <audio ref={btn.name + "_ref"} id={btn.name} className="clip" src="Cymatics - Cobra Clap 12.wav" type="audio/wav"></audio>
+      </button>
+    )
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {pad}
     </div>
   );
 }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keys: ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"]
+    };
+  }
+  onClick = audio => {
+    audio.current.play();
+  }
+  render() {
+    return (
+      <main id="drum-machine">
+        <div id="display">
+          <DrumPad func={() => this.onClick} />
+        </div>
+      </main>
+    )
+  }
+}
+
 
 export default App;
