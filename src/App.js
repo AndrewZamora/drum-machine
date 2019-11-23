c80bc1a6fedfa13a67de4f4125450b6fde35a439
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-11-23 19:08:34 +0900

    Moved drum keys to App's state

--- src/App.js
+++ src/App.js
@@ -1,52 +1,28 @@
 import React, { Component } from 'react'
 import clap_audio from './audio/clap.wav';
 
-const capitalize = (str) => {
+const capitalize = str => {
   return typeof str === 'string' ? str.charAt(0).toUpperCase() + str.slice(1) : '';
 };
 
 class DrumPad extends Component {
+  constructor(props) {
+    super(props);
+    props.keys.forEach(key => {
+      this[`${key}_ref`] = React.createRef();
+    });
+  }
   render() {
-    const { func } = this.props;
-    const drumBtns = [
-      {
-        name: "Q"
-      },
-      {
-        name: "W"
-      },
-      {
-        name: "E"
-      },
-      {
-        name: "A"
-      },
-      {
-        name: "S"
-      },
-      {
-        name: "D"
-      },
-      {
-        name: "Z"
-      },
-      {
-        name: "X"
-      },
-      {
-        name: "C"
-      }
-    ]
-    const pad = drumBtns.map((btn, index) => {
+    const { click, keys } = this.props;
+    const pad = keys.map((key, index) => {
       return (
         <button
           className="drum-pad"
-          id={btn.name + "_btn"}
-          key={index + btn.name}
-          onKeyPress={(event) => func(event, this[btn.name])}
-          onClick={(event) => func(event, this[btn.name])}>
-          {btn.name}
-          <audio ref={audio => (this[btn.name] = audio)} id={btn.name} className="clip" src={clap_audio} type="audio/wav"></audio>
+          id={key + "_btn"}
+          key={index + key}
+          onClick={(event) => click(event, this, key)}>
+          {key}
+          <audio ref={this[`${key}_ref`]} id={key} className="clip" src={clap_audio} type="audio/wav"></audio>
         </button>
       )
     });
@@ -66,22 +42,26 @@ class App extends Component {
       keys: ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"]
     };
   }
-  onClick = async (event, ref) => {
-    console.log(event,ref.id)
-    if (event.key) {
-      console.log(event.key,)
+  handleBtnClick = async (event, ref, name) => {
+    if (event.key && capitalize(event.key) === name) {
+      console.log("YAY")
     }
     try {
-      await ref.play()
+      await ref[`${name}_ref`].current.play()
     } catch (error) {
       console.log(error)
+    }
+  }
+  handleKeyPress = (event, ref, name) => {
+    if (event.key && capitalize(event.key) === name) {
+      console.log("YAY")
     }
   }
   render() {
     return (
       <main id="drum-machine">
         <div id="display">
-          <DrumPad func={(event, ref) => this.onClick(event, ref)} />
+          <DrumPad click={(event, ref, name) => this.handleBtnClick(event, ref, name)} keys={this.state.keys}/>
         </div>
       </main>
     )
