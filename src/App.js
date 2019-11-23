61a5129de6951c4d8d690cd2d1095fc9e983718f
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-11-24 00:15:51 +0900

    Used lifecycle methods to get refs from child and set event listener on doc for key press

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ class DrumPad extends Component {
       this[`${key}_ref`] = React.createRef();
     });
   }
+  componentDidMount() {
+    this.props.sendRefs(this)
+  }
   render() {
     const { click, keys } = this.props;
     const pad = keys.map((key, index) => {
@@ -20,7 +23,7 @@ class DrumPad extends Component {
           className="drum-pad"
           id={key + "_btn"}
           key={index + key}
-          onClick={(event) => click(event, this, key)}>
+          onClick={() => click(key)}>
           {key}
           <audio ref={this[`${key}_ref`]} id={key} className="clip" src={clap_audio} type="audio/wav"></audio>
         </button>
@@ -42,26 +45,50 @@ class App extends Component {
       keys: ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"]
     };
   }
-  handleBtnClick = async (event, ref, name) => {
-    if (event.key && capitalize(event.key) === name) {
-      console.log("YAY")
-    }
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress, false);
+  }
+  getRefs = refs => {
+    const drumKeys = this.state.keys.map(key => {
+      return `${key}_ref`;
+    });
+    const desiredRefs = Object.keys(refs).filter(key => {
+      return drumKeys.includes(key)
+    });
+    desiredRefs.forEach(item => {
+      this.setState({
+        [`${item}`]: refs[`${item}`]
+      })
+    })
+  }
+  handleBtnClick = async (name) => {
     try {
-      await ref[`${name}_ref`].current.play()
+      await this.state[`${name}_ref`].current.play()
     } catch (error) {
       console.log(error)
     }
   }
-  handleKeyPress = (event, ref, name) => {
-    if (event.key && capitalize(event.key) === name) {
-      console.log("YAY")
+  handleKeyPress = async event => {
+    const pressedKey = capitalize(event.key);
+    if (this.state.keys.includes(pressedKey)) {
+      try {
+        await this.state[`${pressedKey}_ref`].current.play()
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
   render() {
     return (
       <main id="drum-machine">
         <div id="display">
-          <DrumPad click={(event, ref, name) => this.handleBtnClick(event, ref, name)} keys={this.state.keys}/>
+          <DrumPad
+            click={(name) => this.handleBtnClick(name)}
+            sendRefs={(ref) => this.getRefs(ref)}
+            keys={this.state.keys} />
         </div>
       </main>
     )
