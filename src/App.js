70f32bb03ad9e083f26aa6476d2680a3d84589a2
Andrew Zamora <andrewmichaelzamora@gmail.com>
2020-02-12 22:21:02 +0900

    Added audio urls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-// AUDIO FILES
-import clap from './audio/clap.wav';
-import cymbals from './audio/Cymatics - Cobra Ride 2.wav';
-import kick from './audio/Cymatics - Cobra Kick 2 - C.wav';
+// AUDIO Tracks are from Cymatics - Cobra Hip Hop Sample Pack => https://cymatics.fm/pages/cobra-hip-hop-sample-pack
+const cymbals = 'https://res.cloudinary.com/diqrplcm8/video/upload/v1581511236/Cymatics_-_Cobra_Ride_2_gthyh0.wav';
+const kick  = 'https://res.cloudinary.com/diqrplcm8/video/upload/v1581511139/Cymatics_-_Cobra_Kick_2_-_C_duddbe.wav' 
+const clap = 'https://res.cloudinary.com/diqrplcm8/video/upload/v1581510946/clap_jxfclx.wav';
+const snares = 'https://res.cloudinary.com/diqrplcm8/video/upload/v1581511643/Cymatics_-_Cobra_Rimshot_Snare_6_n2y8k8.wav';
+const snap = 'https://res.cloudinary.com/diqrplcm8/video/upload/v1581511795/Cymatics_-_Cobra_Snap_5_bfcnnh.wav';
+const percussion = 'https://res.cloudinary.com/diqrplcm8/video/upload/v1581512216/Cymatics_-_Cobra_Percussion_24_qiwrdz.wav';
+const eight08 = 'https://res.cloudinary.com/diqrplcm8/video/upload/v1581512454/Cymatics_-_Cobra_808_16_-_G_qkenis.wav';
+const crash = 'https://res.cloudinary.com/diqrplcm8/video/upload/v1581512826/Cymatics_-_Cobra_Crash_7_jxpgwg.wav';
+const highHat = 'https://res.cloudinary.com/diqrplcm8/video/upload/v1581513237/Cymatics_-_Cobra_Open_Hihat_13_xt0os1.wav';
 // GLOBALS
-const hardCodedKeys = [{ name: "Q", audio: cymbals, description: "cymbals" }, { name: "W", audio: clap, description: "clap" }, { name: "E", audio: kick, description: "kick" }, { name: "A", audio: clap, description: "clap" }, { name: "S", audio: clap, description: "clap" }, { name: "D", audio: clap, description: "clap" }, { name: "Z", audio: clap, description: "clap" }, { name: "X", audio: clap, description: "clap" }, { name: "C", audio: clap, description: "clap" }];
+const hardCodedKeys = [{ name: "Q", audio: cymbals, description: "cymbals" }, { name: "W", audio: clap, description: "clap" }, { name: "E", audio: kick, description: "kick" }, { name: "A", audio: snares, description: "snares" }, { name: "S", audio: snap, description: "snap" }, { name: "D", audio: percussion, description: "percussion" }, { name: "Z", audio: eight08, description: "808" }, { name: "X", audio: crash, description: "crash" }, { name: "C", audio: highHat, description: "highhat" }];
 // COMPONENTS
 class DrumPad extends Component {
   constructor(props) {
